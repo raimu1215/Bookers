7027e7aef695ff68d030f24b452bf85367bfccd3
EC2 Default User <ec2-user@ip-172-31-1-172.ap-northeast-1.compute.internal>
2022-12-05 13:21:52 +0000

    indexページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'new' => 'books#new'
-  get 'index' => 'books#index'
-  get 'show' => 'books#show'
-  get 'edit' => 'books#edit'
+# # get 'new' => 'books#new'
+#   #post 'books' => 'books#create'
+#   get 'index' => 'books#index'
+#   get 'show' => 'books#show'
+#   get 'edit' => 'books#edit'
+  resources :books
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
